8ac70e12767b51af8666f8b8c6b30039135537a6
GGnara <aikokusyanokiwami@gmail.com>
2024-05-17 03:12:05 +0900

    スライダーにサービス表示

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import './Sidebar.css';
 const Sidebar = ({ subValuesArray }) => {
+  const handleClick = (item) => {
+    console.log(item);
+  };
+
   return (
     <div className="sidebar">
       {/* サイドバーの内容 */}
       {subValuesArray.map((item, index) => (
-        <div key={index} className="sidebar-item">{item}</div>
+        <div 
+          key={index} 
+          className="sidebar-item" 
+          onClick={() => handleClick(item)}
+        >
+          {item}
+        </div>
       ))}
       {/* 他の項目 */}
     </div>
